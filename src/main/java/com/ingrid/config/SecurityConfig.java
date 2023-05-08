9991a38e61a7ce99a91a3be938f3e7aa9f8d8cb2
ingridbromer <ingridb@id.uff.br>
2023-05-08 18:13:33 -0300

    bcrypt replaced in passwordEncoder

--- src/main/java/com/ingrid/config/SecurityConfig.java
+++ src/main/java/com/ingrid/config/SecurityConfig.java
@@ -1,46 +1,74 @@
 package com.ingrid.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ingrid.security.jwt.JwtConfigurer;
+import com.ingrid.security.jwt.JwtTokenProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
-	public static final String[] AUTH_WHITELIST = { "/v3/api-docs/**", "/swagger-ui/**"};
-
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
 	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers(AUTH_WHITELIST);
+	PasswordEncoder passwordEncoder() {
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+				
+		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		encoders.put("pbkdf2", pbkdf2Encoder);
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+		return passwordEncoder;
 	}
+	
+    @Bean
+    AuthenticationManager authenticationManagerBean(
+    		AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
-	// @Bean: Expoe o retorno do metodo ao Spring
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		// desabilita ataques contra cross-site request forgery (o token por si só já protege)
-		return http.csrf().disable()
-				// configura a seguranca stateless (não armazena estado a cada requisição)
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-
-	}
-
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+            		session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                    authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(
+							"/auth/signin",
+							"/auth/refresh/**",
+                    		"/swagger-ui/**",
+                    		"/v3/api-docs/**"
+                		).permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/users").denyAll()
+                )
+                .cors()
+                .and()
+                .apply(new JwtConfigurer(tokenProvider))
+                .and()
+                .build();
+ 
+    }
 }
--- src/main/java/com/ingrid/resource/AuthController.java
+++ src/main/java/com/ingrid/resource/AuthController.java
@@ -9,16 +9,21 @@ import com.ingrid.model.dto.TokenVO;
 import com.ingrid.repository.UserRepository;
 import com.ingrid.security.AccountCredentialsVO;
 import com.ingrid.security.jwt.JwtTokenProvider;
+import com.ingrid.service.AuthServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +39,9 @@ public class AuthController {
 	
 	@Autowired
 	UserRepository repository;
+	
+	@Autowired
+	AuthServices authServices;
 	
 	@SuppressWarnings("rawtypes")
 	@PostMapping(value = "/signin", produces = { "application/json", "application/xml", "application/x-yaml" }, 
@@ -62,5 +70,27 @@ public class AuthController {
 		} catch (AuthenticationException e) {
 			throw new BadCredentialsException("Invalid username/password supplied!");
 		}
+	}
+	
+	//Request de refresh token - a cada 1h
+	@SuppressWarnings("rawtypes")
+	@PutMapping(value = "/refresh/{username}")
+	public ResponseEntity refreshToken(@PathVariable("username") String username,
+			@RequestHeader("Authorization") String refreshToken) {
+		if (checkIfParamsIsNotNull(username, refreshToken))
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+		var token = authServices.refreshToken(username, refreshToken);
+		if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+		return token;
+	}
+
+	private boolean checkIfParamsIsNotNull(String username, String refreshToken) {
+		return refreshToken == null || refreshToken.isBlank() ||
+				username == null || username.isBlank();
+	}
+
+	private boolean checkIfParamsIsNotNull(AccountCredentialsVO data) {
+		return data == null || data.getUsername() == null || data.getUsername().isBlank()
+				 || data.getPassword() == null || data.getPassword().isBlank();
 	}
 }
